812b5f0a14813d90e0c19a8cbbeaaa00cdc7da3a
anandmathew9605 <anandmathew9605@gmail.com>
2025-11-05 09:22:20 +0530

    ci: updated web prod roles

--- infra/envs/prod/cicd_web_role.tf
+++ infra/envs/prod/cicd_web_role.tf
@@ -1,26 +1,28 @@
 resource "aws_iam_role" "github_web_prod" {
   name = "github-web-prod"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Federated = "arn:aws:iam::608145123666:oidc-provider/token.actions.githubusercontent.com" }
-        Action    = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            # restrict to PRs to main OR pushes to main, adapt as you like
-            "token.actions.githubusercontent.com:sub" = [
-              "repo:anandmathew9605/project1-serverless:ref:refs/heads/main",
-              "repo:anandmathew9605/project1-serverless:ref:refs/pull/*"
-            ]
-          }
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::608145123666:oidc-provider/token.actions.githubusercontent.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+        },
+        "StringLike": {
+          "token.actions.githubusercontent.com:sub": "repo:anandmathew9605/project1-serverless:*"
         }
       }
-    ]
-  })
+    }
+  ]
+}
+POLICY
 }
 
 resource "aws_iam_role_policy" "github_web_prod_policy" {
@@ -34,7 +36,6 @@ resource "aws_iam_role_policy" "github_web_prod_policy" {
         Effect = "Allow"
         Action = [
           "s3:PutObject",
-          "s3:PutObjectAcl",
           "s3:DeleteObject",
           "s3:ListBucket"
         ]
