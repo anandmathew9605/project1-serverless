2a8bdaff081056c04a309ed52c80691d648a471b
anandmathew9605 <anandmathew9605@gmail.com>
2025-11-06 12:26:14 +0530

    chore(terraform): IAM role for Lambda (dev)

--- infra/envs/dev/roles.tf
+++ infra/envs/dev/roles.tf
@@ -115,3 +115,47 @@ resource "aws_iam_role_policy" "github_deploy_frontend_dev_policy" {
     ]
   })
 }
+
+resource "aws_iam_role" "lambda_role" {
+  name = "project1-serverless-lambda-dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_role_policy" {
+  name = "project1-serverless-lambda-dev"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem",
+          "dynamodb:PutItem",
+          "dynamodb:Query"
+        ],
+        Resource = "arn:aws:dynamodb:ap-south-1:*:table/project1-serverless-visitor-dev"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
+
